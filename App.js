0d23c4d3e3b229c3b3635b6a8c145c01909829bd
Farrukh Atabekov <augini18@gmail.com>
2020-07-03 03:46:40 +0900

    --update:initial commit

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image, Dimensions } from "react-native";
+import {
+  useDimensions,
+  useDeviceOrientation,
+} from "@react-native-community/hooks";
 
 export default function App() {
+  console.log(Dimensions.get("screen"));
+  const { landscape } = useDeviceOrientation();
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{ height: 100, width: 100, backgroundColor: "gold" }}></View>
+      <View
+        style={{ height: 100, width: 100, backgroundColor: "yellow" }}
+      ></View>
+      <View
+        style={{ height: 100, width: 100, backgroundColor: "green" }}
+      ></View>
+      <View
+        style={{ height: 100, width: 100, backgroundColor: "white" }}
+      ></View>
+      <View style={{ height: 100, width: 100, backgroundColor: "grey" }}></View>
     </View>
   );
 }
@@ -14,8 +29,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "coral",
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    alignContent: "center",
+    flexWrap: "wrap",
+  },
+  example: {
+    backgroundColor: "red",
+    width: "100%",
+    height: "30%",
   },
 });
---
+++ styles/GlobalStyles.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+const GlobalStyles = StyleSheet.create({
+  images: {
+    background: require("../assets/images/background.jpg"),
+    chair: require("../assets/images/chair.jpg"),
+    redLogo: require("../assets/images/logo-red.png"),
+  },
+});
+
+export default GlobalStyles;
